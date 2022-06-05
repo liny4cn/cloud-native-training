7e27cf9c45ceb55d3dc5a908676b34a4879329f2
Joseph <ly4cn@126.com>
2022-06-05 20:50:14 +0800

    feat: v6, separate '/healthz' from route '/', added '/debug/*'.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"net/http/pprof"
 	"os"
 	"strings"
 )
@@ -28,23 +29,25 @@ func logRequest(w http.ResponseWriter, r *http.Request) {
 
 	// 3.Server 端记录访问日志包括客户端 IP，HTTP 返回码，输出到 server 端的标准输出
 	remoteIP := getRealIP(r)
+	w.Header().Add("X-Real-Ip", remoteIP)
 	log.Println("Remote Address:", remoteIP)
 
-	// 4. 当访问 localhost/healthz 时，应返回 200
-	if r.RequestURI == "/healthz" {
-		w.WriteHeader(http.StatusOK)
-		io.WriteString(w, "ok")
-		log.Println("Response Status code:", 200)
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		log.Println("Response Status code:", 404)
-	}
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, "Hello world.")
+	log.Println("Response Status code:", 200)
 
 	// 输出 Response header 日志
 	resHeader := w.Header()
 	for key := range resHeader {
-		log.Println(key, resHeader.Get(key))
+		log.Println(key, ":", resHeader.Get(key))
 	}
+}
+
+// 4. 当访问 localhost/healthz 时，应返回 200
+func healthz(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, "ok")
+	log.Println("Response Status code:", 200)
 }
 
 func getRealIP(r *http.Request) string {
@@ -68,6 +71,11 @@ func main() {
 	println("Server is running at 0.0.0.0:80.")
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", logRequest)
+	mux.HandleFunc("/healthz", healthz)
+	mux.HandleFunc("/debug/pprof/", pprof.Index)
+	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
 	err := http.ListenAndServe(":80", mux)
 	if err != nil {
 		log.Fatalf("Server start failed: %v", err.Error())
