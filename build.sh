421c64d0afec7b9bc38549ec697e1622423b4593
Joseph <ly4cn@126.com>
2022-07-24 17:52:50 +0800

    feat: added home work week 8-2

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-docker build -t ly4cn/go-native-cloud:week8 .
+docker build -t ly4cn/go-native-cloud:week3 .
 docker login -u $DOCKER_USER --password-stdin
-docker push ly4cn/go-native-cloud:week8
+docker push ly4cn/go-native-cloud:week3
 docker logout
